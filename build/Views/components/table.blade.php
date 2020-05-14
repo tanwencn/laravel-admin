a1cc941236928f8d9886e62fe9d51175331deb0e
tanwencn <361657055@qq.com>
2020-05-14 13:29:02 +0800

    Update table.blade.php

--- build/Views/components/table.blade.php
+++ build/Views/components/table.blade.php
@@ -5,7 +5,7 @@ $nodata = isset($nodata)?"no-data":"";
 $id = 'table-'.mt_rand(1000, 9999);
 @endphp
 
-<table id="{{ $id }}" style="width: 100%" class="table table-borderless no-pitch {{ $nodata }} {{ $nowrap }} {{ $checkbox }}">
+<table id="{{ $id }}" style="width: 100%" class="table table-hover table-borderless no-pitch {{ $nodata }} {{ $nowrap }} {{ $checkbox }}">
     <thead class="thead-light">
     {{ $thead }}
     </thead>
